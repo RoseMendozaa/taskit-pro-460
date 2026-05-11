de45fb131638a39d4b1980323b783d7c565be9a8
RoseMendozaa <rmendoza3@unm.edu>
2026-05-10 20:03:17 -0600

    Update web-service.js

--- web-service.js
+++ web-service.js
@@ -4,7 +4,6 @@ let uri = process.env.MONGODB_URI || "mongodb://localhost:27017/"
 let client = new MongoClient(uri)
 
 let express = require("express")
-let path = require("path")
 
 let app = express()
 let port = process.env.PORT || 7777
@@ -16,75 +15,45 @@ app.listen(port, function() {
     console.log(`TaskIT server running on port ${port}`)
 })
 
-// TEST ROUTE
 app.get("/helloworld", function (req, res){
     res.send("TaskIT is running.")
 })
 
-// RETRIEVE ALL
 app.get("/retrieve", function (req, res) {
     async function run() {
         try {
             await client.connect()
-
             let database = client.db("app")
             let table = database.collection("tasks")
-
-            let query = {}
-
-            let rows = await table.find(query)
-
+            let rows = await table.find({})
             res.send(JSON.stringify(await rows.toArray()))
-
         } finally {
             await client.close()
         }
     }
-
     run()
 })
 
-// RETRIEVE ONE
 app.get("/retrieve-one/:taskid", function(req, res) {
-
     async function run() {
-
         try {
-
             await client.connect()
-
             let database = client.db("app")
             let table = database.collection("tasks")
-
-            let query = {
-                taskid: parseInt(req.params.taskid)
-            }
-
+            let query = { taskid: parseInt(req.params.taskid) }
             let row = await table.findOne(query)
-
             res.send(JSON.stringify(row))
-
         } finally {
-
             await client.close()
-
         }
-
     }
-
     run()
-
 })
 
-// CREATE
 app.post("/create", function (req, res) {
-
     async function run() {
-
         try {
-
             await client.connect()
-
             let database = client.db("app")
             let table = database.collection("tasks")
 
@@ -98,69 +67,39 @@ app.post("/create", function (req, res) {
                 taskstatus: req.body.taskstatus
             }
 
-            let result = await table.insertOne(record)
-
+            await table.insertOne(record)
             res.send("Task created")
-
         } finally {
-
             await client.close()
-
         }
-
     }
-
     run()
-
 })
 
-// DELETE
 app.delete("/delete/:taskid", function(req, res) {
-
     async function run() {
-
         try {
-
             await client.connect()
-
             let database = client.db("app")
             let table = database.collection("tasks")
-
-            let query = {
-                taskid: parseInt(req.params.taskid)
-            }
-
-            let result = await table.deleteOne(query)
-
+            let query = { taskid: parseInt(req.params.taskid) }
+            await table.deleteOne(query)
             res.send("deleted")
-
         } finally {
-
             await client.close()
-
         }
-
     }
-
     run()
-
 })
 
-// UPDATE
 app.put("/update", function(req, res) {
-
     async function run() {
-
         try {
-
             await client.connect()
-
             let database = client.db("app")
             let table = database.collection("tasks")
 
-            let where = {
-                taskid: parseInt(req.body.taskid)
-            }
+            let where = { taskid: parseInt(req.body.taskid) }
 
             let changes = {
                 $set: {
@@ -172,7 +111,8 @@ app.put("/update", function(req, res) {
                     taskstatus: req.body.taskstatus
                 }
             }
-            let result = await table.updateOne(where, changes)
+
+            await table.updateOne(where, changes)
             res.send("updated")
         } finally {
             await client.close()
